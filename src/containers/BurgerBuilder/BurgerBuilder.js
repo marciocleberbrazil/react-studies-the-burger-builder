4dd65c2b63dd9cf5d4c9c85e023d58b61d2d6170
Marcio Barboza <marciocleberbrazil@gmail.com>
2020-04-13 15:12:23 +1000

    REmoving Ingredients safaly

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -38,12 +38,39 @@ class BurgerBuilder extends Component {
         });
     }
 
+    removeIngredientHandler = type => {
+        const oldCount = this.state.ingredients[type];
+
+        if(oldCount <= 0) return;
+
+        const updatedCount = oldCount - 1;
+        const updatedIngredients = {...this.state.ingredients};
+        updatedIngredients[type] = updatedCount;
+
+        const priceDeduction = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice - priceDeduction;
+
+        this.setState({
+            ingredients: updatedIngredients,
+            totalPrice: newPrice
+        });
+    }
+
     render() {
+        const disabledInfo = {...this.state.ingredients};
+
+        for(let key in disabledInfo) {
+            disabledInfo[key] = disabledInfo[key] <= 0
+        }
+
         return (
             <Auxiliary>
                 <Burger ingredients={this.state.ingredients} />
                 <BurgerControls
-                    ingredientAdded={this.addIngredientHandler} />
+                    ingredientAdded={this.addIngredientHandler}
+                    ingredientRemoved={this.removeIngredientHandler}
+                    disabled={disabledInfo} />
             </Auxiliary>
         );
     }
